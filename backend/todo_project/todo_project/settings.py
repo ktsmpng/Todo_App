94062a5fda08c78ef597984129dc76309b528fa4
Kitty <kitsumpang@gmail.com>
2019-01-02 21:43:20 +0000

    configuring cors, adding middleware and permitted domains

--- backend/todo_project/todo_project/settings.py
+++ backend/todo_project/todo_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     #3rd Party
     'rest_framework',
+    'corsheaders'
 
     #Local
     'todos.apps.TodosConfig',
@@ -52,6 +53,8 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -60,6 +63,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000'
+)
 
 ROOT_URLCONF = 'todo_project.urls'
 
