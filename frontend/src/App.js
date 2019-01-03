b7d6e90d8bd48be90b338fdff1786461ef85bd24
Kitty <kitsumpang@gmail.com>
2019-01-03 14:04:42 +0000

    Installed Axios and hook api to backend django

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-const list = [
-    {
-        "id": 1,
-        "title": "Learn HTTP",
-        "body": "Learning HTTP requests GET and Post"
-    },
-    {
-        "id": 2,
-        "title": "Make React Frontend",
-        "body": "Connecting to API"
-    }
-]
+import axios from 'axios';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      list
-    };
+  state = {
+    todos:[]
+  };
+
+  componentDidMount(){
+    this.getTodos();
+  }
+
+  getTodos(){
+    axios
+      .get('http://127.0.0.1:8000/api/')
+      .then(res => {
+        this.setState({ todos:res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.list.map(item => (
+        {this.state.todos.map(item => (
           <div key={item.id}>
             <h1>{item.title}</h1>
             <p>{item.body}</p>
