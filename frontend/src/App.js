53150e92fb794e510a48a97e89ad3e4c3e16810b
Kitty <kitsumpang@gmail.com>
2019-01-03 13:46:24 +0000

    Initial mock app component with hard coded list which will be switched to api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,24 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+    {
+        "id": 1,
+        "title": "Learn HTTP",
+        "body": "Learning HTTP requests GET and Post"
+    },
+    {
+        "id": 2,
+        "title": "Make React Frontend",
+        "body": "Connecting to API"
+    }
+]
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      list
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.list.map(item => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <p>{item.body}</p>
+          </div>
+          ))}
       </div>
     );
   }
