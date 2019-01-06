bdfd66a6d1358246fa9aa2a85359d924a7f0af29
Kitty <kitsumpang@gmail.com>
2019-01-06 21:50:28 +0000

    Working version of form sending HTTP POST and updating component view

--- backend/todo_project/todos/views.py
+++ backend/todo_project/todos/views.py
@@ -4,7 +4,7 @@ from rest_framework import generics
 from .models import Todo
 from .serializers import TodoSerializer
 
-class ListTodo(generics.ListAPIView):
+class ListTodo(generics.ListAPIView, generics.CreateAPIView):
 	queryset = Todo.objects.all()
 	serializer_class = TodoSerializer
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class App extends Component {
-  state = {
-    todos:[]
-  };
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      todos:[],
+      title: '',
+      body: ''
+    };
+  }
 
   componentDidMount(){
     this.getTodos();
   }
 
-  getTodos(){
+  onChange = (e) => {
+    this.setState({[e.target.name]:e.target.value});
+  }
+
+  getTodos = () =>{
     axios
       .get('http://127.0.0.1:8000/api/')
       .then(res => {
@@ -21,16 +31,49 @@ class App extends Component {
       });
   }
 
-  render() {
+  render(){
     return (
       <div className="App">
+        <h1>Todo App</h1>
+        <TodoForm title={this.state.title} body={this.state.body} onChange={this.onChange} getTodos={this.getTodos}/>
         {this.state.todos.map(item => (
           <div key={item.id}>
-            <h1>{item.title}</h1>
+            <h3>{item.title}</h3>
             <p>{item.body}</p>
           </div>
           ))}
       </div>
+    );
+  }
+
+}
+
+class TodoForm extends App{
+
+  postTodo = (e) => {
+    e.preventDefault();
+    axios
+      .post('http://127.0.0.1:8000/api/', {
+          title: this.props.title,
+          body: this.props.body
+      })
+      .then(res => {
+        alert('sent');
+        this.props.getTodos();
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  render(){
+    return(
+      <form onSubmit={this.postTodo}>
+          <input type="text" name="title" placeholder="Title" onChange={this.props.onChange}/>
+          <input type="text" name="body" placeholder="Description" onChange={this.props.onChange}/>
+          <input type="submit" value="submit" />
+      </form>
     );
   }
 }
